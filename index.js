95af572c22e4a2b0f0edc14a5adeda8884e00d78
Martin Magnone <tinchomagnone23@gmail.com>
2020-11-10 15:14:47 -0300

    controllers, models, index, routes, seeder

---
+++ index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const router = require("./routes");
+
+const app = express();
+const cors = require("cors");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+app.use(router);
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("listening on 8000");
+});
