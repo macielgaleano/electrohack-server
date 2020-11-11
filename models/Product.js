ab57f916ca93079cf3ddbfcac6d0739c55b98559
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-11 09:43:21 -0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+var slugify = require("slugify");
 
 const productSchema = new Schema(
   {
@@ -15,6 +16,16 @@ const productSchema = new Schema(
   },
   { timestamps: true }
 );
+
+adminSchema.pre("save", function (next) {
+  var user = this;
+  if (!user.isModified("slug")) return next();
+]
+      next();
+   
+});
+
+productSchema.slug = slugify(productSchema.name);
 
 const Product = mongoose.model("Product", productSchema);
 
