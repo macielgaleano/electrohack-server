61acbb215daf5fed11b0c0fa0674983152269e4b
Martin Magnone <tinchomagnone23@gmail.com>
2020-11-11 12:47:36 -0300

    delete and update categories

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -3,12 +3,11 @@ const Admin = require("../models/Admin");
 
 module.exports = {
   store: (req, res) => {
-    let hashedPassword = bcrypt.hashSync(req.body.password, 10);
     let admin = new Admin({
       firstname: req.body.firstname,
       lastname: req.body.lastname,
       email: req.body.email,
-      password: hashedPassword,
+      password: req.body.password,
     });
     admin.save();
     res.json({});
--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -5,4 +5,14 @@ module.exports = {
     const categories = await Category.find({});
     return res.json(categories);
   },
+  delete: async (req, res) => {
+    await Category.deleteOne({ name: req.body.name });
+    res.json("Categoría eliminada");
+  },
+  update: async (req, res) => {
+    await Category.findOneAndUpdate(name, {
+      name: req.body.name,
+    });
+    res.json("Categoría actualizada");
+  },
 };
