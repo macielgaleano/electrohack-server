f837042ddb97430c6cad94d0e8366e61b3b0f1ac
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-11 11:24:15 -0300

    Update productController.js

--- controllers/productController.js
+++ controllers/productController.js
@@ -9,7 +9,7 @@ const s3 = new AWS.S3({
   accessKeyId: process.env.AWS_ACCESS_ID,
   secretAccessKey: process.env.AWS_ACCESS_SECRET,
 });
-
+//
 var readOnlyAnonUserPolicy = {
   Version: "2012-10-17",
   Statement: [
