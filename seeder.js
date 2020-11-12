db2ebaeb75ee00039338ecb8dc57e675537349c6
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-12 16:49:01 -0300

    Update seeder.js

--- seeder.js
+++ seeder.js
@@ -35,9 +35,6 @@ const seeder = {
       category.save();
     }
 
-<<<<<<< HEAD
-    console.log(products);
-
     if (await products) {
       //Crar productos
 
@@ -82,62 +79,62 @@ const seeder = {
             order.save();
             user.orders.push(order);
           }
-=======
-    let products = fetch("https://fakestoreapi.com/products/1")
-      .then((res) => res.json())
-      .then((json) => console.log(json));
+          let products = fetch("https://fakestoreapi.com/products")
+            .then((res) => res.json())
+            .then((json) => console.log(json));
 
-    //Crar productos
-    let products_list = [];
-    for (let i = 0; i < products_list.length; i++) {
-      let name = faker.commerce.productName();
-      const product = new Product({
-        name: products_list[i].title,
-        description: products_list[i].description,
-        price: products_list[i].price,
-        brand: faker.random.arrayElement(["Samsung", "Apple", "LG", "Nike"], 4),
-        pictures: [
-          products_list[i].image,
-          faker.image.technics(),
-          faker.image.technics(),
-        ],
-        stock: faker.random.number(),
-        category: faker.random.arrayElement(idCategories, 4),
-        outstanding: faker.random.boolean(),
-        slug: slugify(name),
-      });
-      products.push(product);
-      await product.save();
-    }
-    //For create users
-    let users = [];
-    for (let i = 0; i < 10; i++) {
-      const user = new User({
-        firstname: faker.name.firstName(),
-        lastname: faker.name.lastName(),
-        email: faker.internet.email(),
-        password: "1234",
-        address: faker.address.direction(),
-        phone: faker.phone.phoneNumber(),
-        orders: [],
-      });
-      let number = faker.random.arrayElement([0, 1, 2, 4, 5], 6);
-      if (number > 0) {
-        for (let u = 0; u < number; u++) {
-          const order = new Order({
-            user: user._id,
-            products: await Product.find({}).limit(6),
-            state: "0",
-          });
-          order.save();
-          user.orders.push(order);
->>>>>>> parent of 8e73f58... Puede ser pa
+          //Crar productos
+          let products_list = [];
+          for (let i = 0; i < products_list.length; i++) {
+            let name = faker.commerce.productName();
+            const product = new Product({
+              name: products_list[i].title,
+              description: products_list[i].description,
+              price: products_list[i].price,
+              brand: faker.random.arrayElement(["Samsung", "Apple", "LG", "Nike"], 4),
+              pictures: [
+                products_list[i].image,
+                faker.image.technics(),
+                faker.image.technics(),
+              ],
+              stock: faker.random.number(),
+              category: faker.random.arrayElement(idCategories, 4),
+              outstanding: faker.random.boolean(),
+              slug: slugify(name),
+            });
+            products.push(product);
+            await product.save();
+          }
+          //For create users
+          let users = [];
+          for (let i = 0; i < 10; i++) {
+            const user = new User({
+              firstname: faker.name.firstName(),
+              lastname: faker.name.lastName(),
+              email: faker.internet.email(),
+              password: "1234",
+              address: faker.address.direction(),
+              phone: faker.phone.phoneNumber(),
+              orders: [],
+            });
+            let number = faker.random.arrayElement([0, 1, 2, 4, 5], 6);
+            if (number > 0) {
+              for (let u = 0; u < number; u++) {
+                const order = new Order({
+                  user: user._id,
+                  products: await Product.find({}).limit(6),
+                  state: "0",
+                });
+                order.save();
+                user.orders.push(order);
+              }
+            }
+            user.save();
+            users.push(user);
+          }
         }
       }
-      user.save();
-      users.push(user);
     }
-
     res.json({ adminUser, users, products });
   },
 };
