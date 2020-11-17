627bc650bf646f2f2e3ffa498f8a75e6a36b9afb
Maciel Galeano <macielgaleano.jh@gmail.com>
2020-11-17 12:04:29 -0300

    Update routes.js

--- routes.js
+++ routes.js
@@ -29,10 +29,7 @@ router.patch("/token/logout/admin", authController.adminLogout);
 router.get("/productos", productController.all);
 router.get("/productos/:slug", productController.show);
 router.get("/productos/lista/categorias", categoryController.show);
-router.get(
-  "/productos/categorias/:categoria",
-  productController.showByCategory
-);
+router.get("/productos/categorias/:categoria", productController.showByCategory);
 
 // MIDDLEWARE JWT //
 
